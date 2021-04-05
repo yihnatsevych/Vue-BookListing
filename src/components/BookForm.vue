958083d6b73752b980ec74092ac173ae4d72a9a8
yihnatsevych <yaroslav.ihnatsevych@outlook.com>
2021-04-05 21:40:34 +0300

    Module 5 changes

---
+++ src/components/BookForm.vue
@@ -0,0 +1,25 @@
+<template>
+    <form @submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
+        <input type="text" v-model="bookTitle" name="title" value="" placeholder="Book Title">
+        <input type="text" v-model="bookAuthor" name="author" value="" placeholder="Book Author">
+        <button type="submit" name="button">Add Book</button>
+    </form>
+</template>
+<script>
+export default {
+    name: 'BookForm',
+    props: ['books'],
+    data() {
+        return {
+            bookTitle : '',
+            bookAuthor : ''
+        }
+    },
+    methods: {
+        bookSubmit(bookTitle, bookAuthor) {
+            this.$emit('addBook', bookTitle, bookAuthor);
+        }
+    }
+}
+</script>
+<style></style>
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,16 +4,19 @@
         <ul>
             <book-item :key="book.title" v-for="book in books" :book="book"></book-item>
         </ul>    
+        <book-form @addBook="appendBook"></book-form>
     </div>
 </template>
 <script>
 
 import BookItem from './BookItem';
+import BookForm from './BookForm'
 
 export default {
     name: 'BookList',
     components: {
-        BookItem
+        BookItem,
+        BookForm
     },
     data() {
         return {
@@ -23,6 +26,11 @@ export default {
                 {title: 'American Gods', author: 'Neil Gaiman'},
                 {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
             ]
+        }
+    },
+    methods: {
+        appendBook(bookTitle, bookAuthor) {
+            this.books.push({title: bookTitle, author: bookAuthor});
         }
     }
 }
