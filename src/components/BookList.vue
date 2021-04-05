e7d3b36b90a4bea347ed2643f4ee5947af2458f4
yihnatsevych <yaroslav.ihnatsevych@outlook.com>
2021-04-05 21:11:55 +0300

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         <h1>{{title}}</h1>
+        <ul>
+            <li :key="book.title" v-for="book in books">
+                {{book.title}}:{{book.author}}
+            </li>
+        </ul>    
     </div>
 </template>
 <script>
@@ -8,7 +13,12 @@ export default {
     name: 'BookList',
     data() {
         return {
-            title: 'All Books'
+            title: 'All Books',
+            books: [
+                {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                {title: 'American Gods', author: 'Neil Gaiman'},
+                {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+            ]
         }
     }
 }
@@ -16,5 +26,15 @@ export default {
 <style>
 h1, h2 {
     font-weight: normal;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: block;
+    margin: 0 10px;
 }
 </style>
